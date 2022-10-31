b1c92e848021bfc22488b07549a680c6b0720d18
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2022-10-31 22:01:09 +0530

    Update and rename quicksort.c to QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+//1st ko khada kro ,usse chote ko uske left mai dalo
+//usse bade ko uske right mai arrange kro
+
 void printArray(int *A, int n)
 {
     for (int i = 0; i < n; i++)
@@ -11,7 +14,10 @@ void printArray(int *A, int n)
 
 int partition(int A[], int low, int high)
 {
+    //first element pivot(1st khada kro vala)
     int pivot = A[low];
+
+    //ek aage se i start krte the pivot se
     int i = low + 1;
     int j = high;
     int temp;
@@ -23,11 +29,13 @@ int partition(int A[], int low, int high)
             i++;
         }
 
+        //last walaa bada toh next dekho --
         while (A[j] > pivot)
         {
             j--;
         }
 
+        //swap a[i] a[j]
         if (i < j)
         {
             temp = A[i];
@@ -40,16 +48,20 @@ int partition(int A[], int low, int high)
     temp = A[low];
     A[low] = A[j];
     A[j] = temp;
-    return j;
+    return j; //return high
 }
 
+//class teacher main miss
 void quickSort(int A[], int low, int high)
 {
-    int partitionIndex; // Index of pivot after partition
+    int partitionIndex;
 
+    //run recursively until it has greater than 1 element
     if (low < high)
     {
-        partitionIndex = partition(A, low, high); 
+        //find Index of pivot after partition
+        partitionIndex = partition(A, low, high);
+
         quickSort(A, low, partitionIndex - 1);  // sort left subarray 
         quickSort(A, partitionIndex + 1, high); // sort right subarray
     }
